e6b52e5a4144af4d50fa133502700406c77ef3cd
goodboy998 <57053640+goodboy998@users.noreply.github.com>
2021-03-14 20:57:33 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-var ks=[
-  {name:"怪兽泡泡龙",name_en:"Monster Bubble",img:"/games/monsterbubble-ad.jpg",iosid:144536XXXX},
-  {name:"怪兽泡泡龙2",name_en:"Monster Bubble2",img:"/games/monsterbubble-ad2.jpg",iosid:122507XXXX}
+﻿var ks=[
+  {name:"怪兽泡泡龙",name_en:"Monster Bubble",img:"/games/monsterbubble-ad.jpg",iosid:1445644235},
+  {name:"怪兽泡泡龙2",name_en:"Monster Bubble 2",img:"/games/monsterbubble-ad2.jpg",iosid:1225079351}
 ];
