8724035decb7678f7d8d2c72877cfb7c50251945
goodboy998 <57053640+goodboy998@users.noreply.github.com>
2021-03-14 10:43:34 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,4 @@
+var ks=[
+  {name:"怪兽泡泡龙",name_en:"Monster Bubble",img:"/games/monsterbubble-ad.jpg",iosid:144536XXXX},
+  {name:"怪兽泡泡龙",name_en:"Monster Bubble",img:"/games/monsterbubble-ad2.jpg",iosid:122507XXXX}
+];
