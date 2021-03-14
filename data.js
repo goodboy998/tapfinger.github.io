3c57c2f4964cb77576952b1c7ffaa011fa92ad53
goodboy998 <57053640+goodboy998@users.noreply.github.com>
2021-03-14 20:59:02 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,3 @@
 ﻿var ks=[
-  {name:"怪兽泡泡龙",name_en:"Monster Bubble",img:"/games/monsterbubble-ad.jpg",iosid:1445644235},
-  {name:"怪兽泡泡龙2",name_en:"Monster Bubble 2",img:"/games/monsterbubble-ad2.jpg",iosid:1225079351}
+  {name:"怪兽泡泡龙",name_en:"Monster Bubble",img:"/games/monsterbubble-ad.jpg",iosid:1445644235}
 ];
