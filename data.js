b897f7c53ad876a8bc6bba73240725ec8ecb31e9
goodboy998 <57053640+goodboy998@users.noreply.github.com>
2023-02-18 12:30:46 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,3 +1,18 @@
 ﻿var ks=[
-  {name:"经典怪物泡泡消除",name_en:"Monster Pop Bubble Shooter",img:"/games/monsterbubble-ad.jpg",iosid:1558460271}
+  {name:"解密方块连连看飞行棋跳棋井字棋",name_en:"Onet Connect Wood Block Puzzle",img:"/games/onetconnectwoodblockpuzzle-ad.jpg",iosid:1561426939},
+  {name:"萌宠粉碎消消消",name_en:"Toy Smash Fever",img:"/games/toysmashfever-ad.jpg",iosid:1670498425},
+  {name:"萌宠疯狂连连消 - 开心动感消除小游戏",name_en:"Pet Crush Blast Mania",img:"/games/petcrushblastmania-ad.jpg",iosid:1671383093},
+  {name:"泡泡龙经典消消乐单机泡泡大作战游戏",name_en:"Bubble Shooter - Tap Finger",img:"/games/bubbleshootertapfinger-ad.jpg",iosid:6443952324},
+  {name:"经典怪物泡泡消除",name_en:"Monster Pop Bubble Shooter",img:"/games/monsterbubble-ad.jpg",iosid:1558460271},
+  {name:"开心水果连连看 - 水果消除小游戏",name_en:"Fruit Frenzy Match Game",img:"/games/fruitfrenzymatchgame-ad.jpg",iosid:1671070267},
+  {name:"点指开心水果糖消消消",name_en:"Sweet Candy Blast Fever",img:"/games/sweetcandyblastfever-ad.jpg",iosid:1669398277},
+  {name:"经典物理游戏-弹弹球碎砖块",name_en:"Brick Breaker Smash Ball",img:"/games/brickbreakersmashball-ad.jpg",iosid:6443995809},
+  {name:"指尖火柴人滑雪板冲浪滑雪",name_en:"Stickman Snowboard Sports",img:"/games/stickmansnowboardsports-ad.jpg",iosid:1665811721},
+  {name:"指尖网球运动冠军大赛",name_en:"Finger Tennis Sports Game",img:"/games/fingertennissportsgame-ad.jpg",iosid:1664613342},
+  {name:"飞行棋跳棋经典方块拼图",name_en:"Ludo Classic Block Puzzle",img:"/games/ludoclassicblockpuzzle-ad.jpg",iosid:1658222410},
+  {name:"经典益智方块拼图",name_en:"Block Fit Puzzle",img:"/games/blockfitpuzzle-ad.jpg",iosid:6444055995},
+  {name:"经典记忆游戏连连看2048",name_en:"Memory 2048",img:"/games/memory2048-ad.jpg",iosid:6444296487},
+  {name:"解密方块连连看飞行棋跳棋井字棋",name_en:"Onet Connect Wood Block Puzzle",img:"/games/onetconnectwoodblockpuzzle-ad.jpg",iosid:1561426939},
+  {name:"点指连连看—经典小游戏",name_en:"Tap Finger Fruit Link Speed",img:"/games/tapfingerfruitlinkspeed-ad.jpg",iosid:1618936673},
+  {name:"卡罗姆—经典小游戏",name_en:"Tap Fingers Pocket Carrom",img:"/games/tapfingerspocketcarrom-ad.jpg",iosid:1616134844}
 ];
