51288ab9e910e2a87c0445093c1f01c98d1e809e
goodboy998 <57053640+goodboy998@users.noreply.github.com>
2023-02-19 19:28:32 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 ﻿var ks=[
   {name:"解密方块连连看飞行棋跳棋井字棋",name_en:"Onet Connect Wood Block Puzzle",img:"/games/onetconnectwoodblockpuzzle-ad.jpg",iosid:1561426939},
   {name:"经典怪物泡泡消除",name_en:"Monster Pop Bubble Shooter",img:"/games/monsterbubble-ad.jpg",iosid:1558460271},
-  {name:"开心水果连连看 - 水果消除小游戏",name_en:"Fruit Frenzy Match Game",img:"/games/fruitfrenzymatchgame-ad.jpg",iosid:1671070267}
-  ];
+  {name:"点指连连看—经典小游戏",name_en:"Tap Finger Fruit Link Speed",img:"/games/tapfingerfruitlinkspeed-ad.jpg",iosid:1618936673}
+];
